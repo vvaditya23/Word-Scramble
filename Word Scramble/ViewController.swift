9288a6c95b74bb0341d136d4223a3ced8c5d021f
Aditya Vyavahare <vvaditya23@icloud.com>
2023-12-05 22:24:09 +0530

    configured data source for tableview.

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var allWords = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Loaded")
+        if let startWordsURL = Bundle.main.url(forResource: "words", withExtension: ".txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["error"]
+        }
+        
+        startGame()
     }
-
-
 }
 
+extension ViewController {
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+}
