3bb2a4c966a0cbd21b1c2cc8f77456d3f8d3953b
Aditya Vyavahare <vvaditya23@icloud.com>
2023-12-07 14:16:47 +0530

    added check of input size to be greater than 2.

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -106,10 +106,14 @@ extension ViewController {
     
     //checks the I/P word with dictionary (objC)
     func isReal(word: String) -> Bool {
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        return misspelledRange.location == NSNotFound
+        if word.count < 3 {
+            return false
+        } else {
+            let checker = UITextChecker()
+            let range = NSRange(location: 0, length: word.utf16.count)
+            let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+            return misspelledRange.location == NSNotFound
+        }
     }
 }
 
